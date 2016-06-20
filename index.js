1238a1b9784b2d3a6038f405356ff32f8b84ced2
Sean Dou <douguohui@gmail.com>
2016-06-20 17:02:24 +0800

    basic repl

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 'use strict';
 
+module.exports = function(options) {
+
+  let repl = require('repl');
+  let replServer = repl.start('> ');
+
+  replServer.on('exit', () => {
+    process.exit();
+  });
+
+};

764795a2c6d1800c8ec5587ba0f91892f207b6af
Sean Dou <douguohui@gmail.com>
2016-06-23 17:15:52 +0800

    overwrite repl.clear

--- plugins/history.js
+++ plugins/history.js
@@ -14,11 +14,8 @@ module.exports = function(repl, file) {
   let fd = fs.openSync(file, 'a');
 
   repl.rli.addListener('line', function(code) {
-    if (code && code !== '.history') {
+    if (code && ['.history', '.clear'].indexOf(code) == -1) {
       fs.write(fd, code + '\n');
-    } else {
-      repl.rli.historyIndex++;
-      repl.rli.history.pop();
     }
   });
 
@@ -33,7 +30,18 @@ module.exports = function(repl, file) {
       repl.rli.history.forEach(function(v, k) {
         out.push(v);
       });
+
       repl.outputStream.write(out.reverse().join('\n') + '\n');
+      repl.displayPrompt();
+    }
+  });
+
+  repl.defineCommand('clear', {
+    help : 'Clear history',
+    action : function() {
+      fs.writeFileSync(file, '');
+      repl.rli.history = [];
+      repl.rli.historyIndex = -1;
       repl.displayPrompt();
     }
   });
